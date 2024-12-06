53981d1c6b57975cd1c167e2589fc16fcdbb0a32
JayakrishnaTechie <gjkrish521@gmail.com>
2024-12-06 17:37:24 +0530

    Episode-1

---
+++ first.js
@@ -0,0 +1,17 @@
+/* <div id='parent'>
+    <div id='children'>
+        <div id='child1'> <h1>This is H1 Tag</h1></div>
+        <div id='child2'><h2>This is H2 Tag</h2> </div>
+    </div>
+</div> */
+
+const parent=React.createElement(
+    'div',
+    {id:'parent'},
+    React.createElement('div',{id:'children'},[
+        React.createElement('div',{id:'child1'},'This is H1 Tag'),
+        React.createElement('div',{id:'child2'},'This is H2 Tag')
+ ])
+);
+ const root=ReactDOM.createRoot(document.getElementById('root'));
+ root.render(parent)
