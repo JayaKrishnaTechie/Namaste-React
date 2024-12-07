fb9f1c79d1ed529711474f4e30d7a0b982eb0fef
JayakrishnaTechie <gjkrish521@gmail.com>
2024-12-07 09:49:59 +0530

    changed

--- first.js
+++ first.js
@@ -12,8 +12,7 @@ const parent=React.createElement(
         React.createElement('div',{id:'child1'},'This is H1 Tag'),
         React.createElement('div',{id:'child2'},'This is H3 Tag'),
         React.createElement('div',{id:'child2'},'This is H2 Tag'),
-        React.createElement('div',{id:'child2'},'This is H2 Tag'),
-        React.createElement('div',{id:'child2'},'This is H2 Tag'),
+        
 ])
 );
  const root=ReactDOM.createRoot(document.getElementById('root'));
